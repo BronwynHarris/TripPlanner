33e05baf9e5a98ab3d63adc8823ba27da41b5713
Katherine Pangtay <kpangtay@gmail.com>
2018-05-01 17:40:05 -0400

    last step

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const mapboxgl = require("mapbox-gl");
+const buildMarker = require("./marker");
+
+const marker = buildMarker("activity", [-74.009151, 40.705086]); // or [-87.6354, 41.8885]
+marker.addTo(map);
 
 mapboxgl.accessToken = "pk.eyJ1IjoiYnJvbnd5bmgiLCJhIjoiY2pnbzU1amlkMm03YzJxbWozajZmaGI1eiJ9.d8RmVOMJFMq_vI9gpGfo0Q";
 
@@ -9,3 +13,10 @@ const map = new mapboxgl.Map({
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
 
+
+const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map); // [-87.641, 41.895] for Chicago
---
+++ src/marker.js
@@ -0,0 +1,16 @@
+const mapbox = require("mapbox-gl");
+
+const iconURLs = {
+    hotels: "http://i.imgur.com/D9574Cu.png",
+    restaurants: "http://i.imgur.com/cqR6pUI.png",
+    activities: "http://i.imgur.com/WbMOfMl.png"
+};
+
+const buildMarker = (type, coords) =&gt; {
+    // Your Code Here
+    if(type === hotels){
+
+    }
+};
+
+module.exports = buildMarker;
